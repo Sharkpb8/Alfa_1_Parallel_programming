9e47916f3b01283b5431032867ee2f21b1e9aaeb
sharkpb8 <sharkpb8@gmail.com>
2024-12-11 13:49:25 +0100

    choose class

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 playerclasses = ["wizard"]
 
-classchoosen = False
-
-while not classchoosen:
+playerclass = None
+while not playerclass:
     count =1
     print("Vyberte si svojí hráčskou classu")
     for i in playerclasses:
         print(f"{count}. {i}")
 
     playerchoice = input("Vybírám si: ")
+    if(isinstance(int(playerchoice),int) and int(playerchoice) <= len(playerclasses)):
+        playerclass = playerclasses[int(playerchoice)-1]
+    elif(playerchoice in playerclasses):
+        playerclass = playerchoice
+        
