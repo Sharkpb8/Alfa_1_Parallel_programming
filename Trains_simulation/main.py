d65b1cb37e034e3468b8ca240223d8ab877debc8
Sharkpb8 <sharkpb8@gmail.com>
2024-12-14 20:43:17 +0100

    choices in main added

--- Trains_simulation/main.py
+++ Trains_simulation/main.py
@@ -65,13 +65,13 @@ def addstation(list):
             t.addstation(station)
             break
         except ValueError:
-            # trainlist = []
-            # for i in list:
-            #     trainlist.append(f"{list[count-1].type}{list[count-1].train_number}")
-            # if(choice in trainlist):
-            #     t = list[numberchoise-1]
-            #     break
-            print("dej tam cislo jsem linej to tet implementovat")
+            trainlist = []
+            for i in list:
+                trainlist.append(f"{list[count-1].type}{list[count-1].train_number}")
+            if(choice in trainlist):
+                t = list[numberchoise-1]
+                break
+            # print("dej tam cislo jsem linej to tet implementovat")
         print("Špatná volba")
     
 
@@ -82,25 +82,31 @@ def addstation(list):
 running = True
 trainlist = []
 while running:
-    options = ["Pridat vlak","Přidat zastavku vlaku","Výpis vlaku","Ukončit"]
+    options = ["Pridat vlak","Smazat vlak","Přidat zastavku vlaku","Smazat zastavku vlaku","Výpis vlaku","Spustit simulaci","Ukončit"]
     showoptions(options)
     choice = input("Vybírám si: ")
     match choice:
         case "Pridat vlak" | "1":
             t = addtrain()
             trainlist.append(t)
-        case "Přidat zastavku vlaku" | "2":
+        case "Smazat vlak" | "2":
+            raise NotImplementedError
+        case "Přidat zastavku vlaku" | "3":
             if(len(trainlist) == 0):
                 print("Žádny vlaky nebyly vytvořeny")
             else:
                 station = addstation(trainlist)
-        case "Vypis vlaku" | "3":
+        case "Smazat zastavku vlaku" | "4":
+            raise NotImplementedError
+        case "Vypis vlaku" | "5":
             if(len(trainlist) == 0):
                 print("Žádny vlaky nebyly vytvořeny")
             else:
                 for i in trainlist:
                     print(i)
-        case "Ukončit" | "0":
+        case "Spustit simulaci" | "6":
+            raise NotImplementedError
+        case "Ukončit" | "7":
             running = False
         case _:
             print("Špatná volba")
